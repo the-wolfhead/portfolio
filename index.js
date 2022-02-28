6416dc57896785edaf507e3d5d216f51602d493d
Daniel Otabor <danieldamianotabor@gmail.com>
2022-02-28 11:32:17 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.use('/', router);
+app.use(express.static(path.join(__dirname, "/images")));
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+app.listen(PORT, LOCAL_ADDRESS, () => {
+  
+});
+
+
+module.exports = app;
