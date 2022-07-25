2ce37264d3c554a85fd71289a55cf67fec8f4fd8
Daniel Otabor <danieldamianotabor@gmail.com>
2022-07-25 03:05:34 +0100

    updates

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
+
+router.get('/blog',function(req,res){
+  res.sendFile(path.join(__dirname+'/blog.html'));
+  //__dirname : It will resolve to your project folder.
+});
 app.use('/', router);
 app.use(express.static(path.join(__dirname, "/images")));
 const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
