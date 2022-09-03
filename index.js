56e6ca8a909ff94a29fc93301409084f9f0322f2
Daniel Otabor <danieldamianotabor@gmail.com>
2022-09-03 10:54:35 +0100

    adding body-parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bodyparser = require('body-parser');
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
